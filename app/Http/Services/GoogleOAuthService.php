b7744218af73684983c387a84ba61d4ad8f360d8
Dok2314 <koversundanil@gmail.com>
2023-08-14 17:08:07 +0300

    Changes

---
+++ app/Http/Services/GoogleOAuthService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Services;
+
+class GoogleOAuthService
+{
+    private string $clientId = '17045592431-4n68fvp5d442iv338ubd0u0hcdtug5i3.apps.googleusercontent.com';
+    private string $clientSecret = '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';
+
+
+}
